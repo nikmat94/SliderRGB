1538ea8fb477c18673c3ae7585b6f4d5e287b5a2
MatveenkovNikita <matveenkoff@gmail.com>
2021-03-26 21:50:26 +0300

    launchView

--- SliderRGB/ViewController.swift
+++ SliderRGB/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redAlphaLabel: UILabel!
+    @IBOutlet var greenAlphaLabel: UILabel!
+    @IBOutlet var blueAlphaLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private var red: CGFloat = 0.0
+    private var green: CGFloat = 0.0
+    private var blue: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 10
+        red = CGFloat(redSlider.value)
+        green = CGFloat(greenSlider.value)
+        blue = CGFloat(blueSlider.value)
+        
+        redAlphaLabel.text = String(redSlider.value)
+        greenAlphaLabel.text = String(greenSlider.value)
+        blueAlphaLabel.text = String(blueSlider.value)
+        
+        rgbView.backgroundColor = UIColor(displayP3Red: red, green: green, blue: blue, alpha: 1)
     }
-
-
+    
 }
+
+    
 
